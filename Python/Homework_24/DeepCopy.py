b0759636bd98137da766dc75b121b723c12be000
vadymzhernovoi <zhernovoivadym@gmail.com>
2025-05-25 10:36:02 +0200

    modified file Python/Homework_24/DeepCopy.py

--- Python/Homework_24/DeepCopy.py
+++ Python/Homework_24/DeepCopy.py
@@ -14,43 +14,45 @@ original_data = [
 '''
 from copy import deepcopy
 
-original_data = [
- [1, 2, 3], # Вложенный список
- (4, [5, 6], {7, 8}), # Кортеж с вложенными структурами
- {"a": 9, "b": [10, 11]}, # Словарь со списком
- "Hello", # Строка
- [12, (13, 14)], # Список с кортежем
- 15.5, # Число с плавающей точкой
- 5 # Целое число
-]
-def my_deep_copy(items: list[any]) -> list[any]:
+def my_deep_copy(objects: any) -> any:
     '''
     Аналог deepcopy() с помощью рекурсии.
-    :param items: коллекция с любым количеством любой вложенности любых объектов.
+    :param objects: коллекция с любым количеством любой вложенности любых объектов.
     :return: независимая копия коллекции
     '''
-    #print(items)
-    if isinstance(items, list | set | tuple | dict):
-        if isinstance(items, list | set | tuple):
+    #print(objects)
+    if isinstance(objects, list | set | tuple | dict):
+        if isinstance(objects, list | set | tuple):
             new_data = list()
-            for item in items:
+            for item in objects:
                 new_data.append(my_deep_copy(item))
-            if isinstance(items, set):
+            if isinstance(objects, set):
                 new_data = set(new_data)
-            elif isinstance(items, tuple):
+            elif isinstance(objects, tuple):
                 new_data = tuple(new_data)
         else:
             new_data = dict()
-            for key, value in items.items():
+            for key, value in objects.items():
                 new_data[key] = my_deep_copy(value)
         return new_data
     else:
-        return items
+        return objects
     return new_data
 
+
+original_data = [
+    [1, 2, 3],  # Вложенный список
+    (4, [5, 6], {7, 8}),  # Кортеж с вложенными структурами
+    {"a": 9, "b": [10, 11]},  # Словарь со списком
+    "Hello",  # Строка
+    [12, (13, 14)],  # Список с кортежем
+    15.5,  # Число с плавающей точкой
+    5  # Целое число
+]
 print('        original_data:', original_data)
 new_data = my_deep_copy(original_data)
 print('             new_data:', new_data)
+# проверяем
 new_data[1][1].append({1,2,3})
 new_data[2]["b"] = [12, 13, 14, 15]
 new_data[3] = 'DeepCopy'
