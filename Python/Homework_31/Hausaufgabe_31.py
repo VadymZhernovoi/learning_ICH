0eded5ad3e54bea99d4325f168c1d13751097c8c
vadymzhernovoi <zhernovoivadym@gmail.com>
2025-06-19 17:08:45 +0200

    added Hausaufgabe_31.py

---
+++ Python/Homework_31/Hausaufgabe_31.py
@@ -0,0 +1,48 @@
+'''
+1.  Извлечение дат
+    Реализуйте программу, которая должна:
+    Найти в тексте все даты в форматах DD/MM/YYYY, DD-MM-YYYY и DD.MM.YYYY.
+Данные:
+text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
+Пример вывода:
+15/03/2025
+01.12.2024
+09-09-2023
+28/02/2022
+'''
+import re
+
+text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
+print("итератор finditer:")
+matches = re.finditer(r"\d{2}/\d{2}/\d{4}|\d{2}-\d{2}-\d{4}|\d{2}\.\d{2}\.\d{4}", text)
+for match in matches:
+    print(match.group())
+
+print("список findall:")
+matches = re.findall(r"\d{2}/\d{2}/\d{4}|\d{2}-\d{2}-\d{4}|\d{2}\.\d{2}\.\d{4}", text)
+for match in matches:
+    print(match)
+
+'''
+2.  Разделение списка тегов
+    Реализуйте программу, которая должна:
+    Прочитать строку с тегами, введёнными пользователем.
+    Разделить её на отдельные теги, независимо от того, чем они были разделены (запятые, точки с запятой, слэши или пробелы).
+    Удалить лишние пробелы и пустые значения.
+Данные:
+tag_input = "python, data-science / machine-learning; AI neural-networks"
+Пример вывода:
+['python', 'data-science', , 'machine-learning', 'AI', 'neural-networks']
+'''
+tag_input = "python  ,, data-science   // , ; / machine-learning; AI neural-networks"
+print()
+print("findall:")
+matches = re.findall(r"[^,;/\s]+", tag_input)
+print(matches)
+# Удалить лишние пробелы и пустые значения.
+matches = [tag.strip() for tag in re.findall(r"[^,;/\s]+", tag_input) if tag] # хотя это лишнее - '\s' и '+' и так не дают лишних пробелов и пустых значений
+print(matches)
+
+print("split:")
+matches = re.split(r"[,;/\s]+", tag_input)
+print(matches)
